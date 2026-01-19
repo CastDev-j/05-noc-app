522cfe32299ec56d8a8ebf1cd39e2a4f33343164
AndDev <23031429@itcelaya.edu.mx>
2026-01-19 15:45:08 -0600

    feat: integrate logging repository with FileSystemDataSource in ServerApp

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,7 @@
 import { CheckService } from "@/domain/use-cases/checks/check-service";
 import { CronService } from "./cron/cron-service";
+import { LogRepositoryImpl } from "@/infrastructure/repositories/log";
+import { FileSystemDataSource } from "@/infrastructure/datasources/file-system";
 
 const successCallback = () => {
   console.log("Service is reachable");
@@ -9,12 +11,15 @@ const errorCallback = (error: Error) => {
   console.error("Service is not reachable");
 };
 
+const logRepository = new LogRepositoryImpl(new FileSystemDataSource());
+
 export class ServerApp {
   static start(): void {
     CronService.createJob({
       cronTime: "*/5 * * * * *",
       onTick: async () => {
         const isReachable = await new CheckService(
+          logRepository,
           successCallback,
           errorCallback,
         ).execute("http://localhost:3000");
